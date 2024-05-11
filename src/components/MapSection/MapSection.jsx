e3b655b725fe291135654175f256551f183a1225
Claude Jordan <claudejordan@gmail.com>
2024-05-11 18:53:22 +0200

    updated styling to make it more uniform, added short explainer text that map and cards are clickable to highlight

--- src/components/MapSection/MapSection.jsx
+++ src/components/MapSection/MapSection.jsx
@@ -20,43 +20,60 @@ const MapSection = ({
   const [viewState, setViewState] = useState({
     latitude: 50.916095041454554,
     longitude: 6.960644911005172,
-    zoom: 11,
+    zoom: 10,
   });
 
+  if (!processedGasStationData) {
+    return (
+      <div className="container" style={{ textAlign: "center" }}>
+        Lade Karte...
+      </div>
+    );
+  }
+
   return processedGasStationData ? (
-    <div className="map-container">
-      <Map
-        {...viewState}
-        mapStyle="mapbox://styles/mapbox/outdoors-v12"
-        mapboxAccessToken={mapboxApiKey}
-        onMove={(event) => setViewState(event.viewState)}
-        style={{ width: 800, height: 600 }}
-      >
-        <GeocoderControl
+    <div>
+      <div className="map-explanation">
+        <p>
+          Durch Klick auf das Tankstellensymbol wird die Tankstelle in der{" "}
+          <br />
+          Tabelle hervorgehoben und umgekehrt.
+        </p>
+      </div>
+      <div className="map-container">
+        <Map
+          {...viewState}
+          mapStyle="mapbox://styles/mapbox/outdoors-v12"
           mapboxAccessToken={mapboxApiKey}
-          position="top-right"
-        />
-        {processedGasStationData.map((gasStation) => (
-          <Marker
-            key={gasStation.attributes.objectid}
-            latitude={gasStation.geometry.y}
-            longitude={gasStation.geometry.x}
-            onClick={() =>
-              setCurrentGasStationId(gasStation.attributes.objectid)
-            }
-          >
-            <img
-              alt="fuel pump"
-              className={
-                gasStation.attributes.objectid === currentGasStationId
-                  ? "active"
-                  : ""
+          onMove={(event) => setViewState(event.viewState)}
+          style={{ width: 800, height: 600 }}
+        >
+          <GeocoderControl
+            mapboxAccessToken={mapboxApiKey}
+            position="top-right"
+          />
+          {processedGasStationData.map((gasStation) => (
+            <Marker
+              key={gasStation.attributes.objectid}
+              latitude={gasStation.geometry.y}
+              longitude={gasStation.geometry.x}
+              onClick={() =>
+                setCurrentGasStationId(gasStation.attributes.objectid)
               }
-              src={pump}
-            />
-          </Marker>
-        ))}
-      </Map>
+            >
+              <img
+                alt="fuel pump"
+                className={
+                  gasStation.attributes.objectid === currentGasStationId
+                    ? "active"
+                    : ""
+                }
+                src={pump}
+              />
+            </Marker>
+          ))}
+        </Map>
+      </div>
     </div>
   ) : (
     <div />
